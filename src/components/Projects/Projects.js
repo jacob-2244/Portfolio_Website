a4326eed5c384dfaa2e80ee583bcc7aad73b6c3c
Muhammad Yaqoob <muhammadyaqoob2580@gmail.com>
2025-10-21 18:30:00 +0500

    added recent projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import Dep from "../../Assets/Projects/DeP.jpeg";
-import YumSpot from "../../Assets/Projects/YumSpot.png";
-import notekeeper from "../../Assets/Projects/notekeeper.png";
+import Echofy from "../../Assets/Projects/echofy.png";
+import Portfolite from "../../Assets/Projects/portfolite.png";
 import emotions from "../../Assets/Projects/emotions.jpg";
 import eyecare from "../../Assets/Projects/eyecare.png";
 import weather from "../../Assets/Projects/weather.jpeg"; 
+import Ecommerce from "../../Assets/Projects/Ecommerce.png"; 
+
+
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -22,12 +24,13 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={Dep}
+              imgPath={Echofy}
               isBlog={false}
-              title="DEP"
-              description="Digital Empowerment Pakistan is a platform where individuals can apply for internships, leadership programs, and other opportunities to enhance their skills. Built with Next.js, Material-UI, and Firebase, the platform enables real-time interactions, resource sharing, and seamless communication to empower aspiring professionals."
-              ghLink="https://github.com/jacob-2244/DigitalEmpowermentPakistan"
-              // demoLink="https://chatify-49.web.app/"
+              title="Echofy(Animated Website)"
+              description="Echofy: Fully Animated Environmental Awareness Website 💚
+              A fully animated, fully responsive website built to promote environmental awareness and sustainability 🌎"
+              ghLink="https://github.com/jacob-2244/green_guard.git"
+              demoLink="https://full-animated-website-seven.vercel.app/"
             />
           </Col>
 
@@ -38,29 +41,29 @@ function Projects() {
               title="Federated Learning Cataract Detection"
               description="Detect Your Eye Disease Using Federated Learning where your data is not shared with anyone. The model is trained on your local device and the results are sent to the server without sharing your data. system is developed on nextjs, for database we are using firebase and for the model we are using federated learning."
               ghLink="  https://github.com/jacob-2244/Federated_Learning_Cataract_Detection.git"
-              // demoLink="https://chatify-49.web.app/"
+              demoLink="https://federated-cataract.vercel.app/"
             />
           </Col>
 
            <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={YumSpot}
+              imgPath={Ecommerce}
               isBlog={false}
-              title="YumSpot"
-              description="YumSpot is a full-stack restaurant reservation web application built using the MERN stack (MongoDB, Express.js, React.js, Node.js). Users can browse restaurants, reserve tables, and manage bookings online."
-              ghLink="https://github.com/jacob-2244/YumSpot.git"
-              // demoLink="https://chatify-49.web.app/"
+              title="E-Commerce Store"
+              description="An Ecommerce Store which takes data from api and we are implementing logic add to cart using redux toolkit"
+              ghLink="https://github.com/jacob-2244/Ecommerce-Store.git"
+              demoLink="https://ecommerce-store-gold-chi.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={notekeeper}
+              imgPath={Portfolite}
               isBlog={false}
-              title="NoteKeeper APP"
-              description="NoteKeeper is a simple and intuitive web application to keep your daily notes organized. Whether it’s a to-do list, reminder, or personal idea, NoteKeeper helps you manage your notes efficiently in a clean user interface."
-              ghLink="https://github.com/jacob-2244/NoteKeeperApp.git"
-              // demoLink="https://chatify-49.web.app/"
+              title="Portfolite"
+              description="A animated website designed using next.js,typescript and used many animated libraries like framer-motion and used shadecn and other UI libraries"
+              ghLink="https://github.com/jacob-2244/portfolite.git"
+               demoLink="https://yaqoobportfolite.vercel.app/"
             />
           </Col>
 
