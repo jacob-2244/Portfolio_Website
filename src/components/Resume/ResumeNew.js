372b98465fc8a4625cde92d5cb7d3386a45bea2d
Muhammad Yaqoob <muhammadyaqoob2580@gmail.com>
2025-11-26 00:51:46 +0500

    updated resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Updated resume mern .pdf";
+import pdf from "../../Assets/M.yaqoob(mern revised).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
